8e45174cf7f8e34e33753f86d7b72da8ed646189
hnt04 <105774543+hnt04@users.noreply.github.com>
2022-07-28 10:27:57 +0700

    update-2

--- src/useTimer.js
+++ src/useTimer.js
@@ -1,21 +1,29 @@
 import { useState, useRef } from "react";
 
 const useTimer = (ini = 0) => {
-  const [time, setTime] = useState("0:00:00");
+  const [time, setTime] = useState(ini);
 
-  const isStart = "Your code here";
-  const active = useRef(0);
+  const isStart = useRef(0);
+  const active = useRef(true);
   const refInterval = useRef();
 
   const startTimer = () => {
-    "Your code here";
+    refInterval.current=setInterval(() => {
+      isStart.current++;
+      setTime(prev => prev + 1)
+    }, 1000)
     active.current.disabled = true;
   };
   const stopTimer = () => {
-    "Your code here";
+    clearInterval(refInterval.current);
+    refInterval.current = 0
   };
   const resetTimer = () => {
-    "Your code here";
+    stopTimer();
+    if(time) {
+      refInterval.current++;
+      setTime(0)
+    }
     active.current.disabled = false;
   };
 
