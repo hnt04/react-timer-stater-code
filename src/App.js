3050ad817459041fdf07e645e0aae7495477e784
hnt04 <105774543+hnt04@users.noreply.github.com>
2022-07-28 17:27:05 +0700

    update-8

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import useTimer from "./useTimer";
 function App() {
   const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
 
+  const formatTime = () => {
+    const getSeconds = `0${(time % 60)}.slide(-2)`
+    const minutes = `${Math.floor(time/60)}`
+    const getMinutes = `${minutes % 60}.slide(-2)`
+    const getHours = `0${Math.floor(time/3600)}`
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`
+  }
+
   return (
     <div className="App container">
       <h1>Coder Timer</h1>
       <div className="timer__wrapper">
         <div className="timer__display">
-          <p>{time}</p>
+          <p>{formatTime}</p>
         </div>
         <div className="button__wrapper">
           <button className="button" onClick={stopTimer}>
