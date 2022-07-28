72b920bb6d7ef34202352417097784c07f96057d
hnt04 <105774543+hnt04@users.noreply.github.com>
2022-07-28 17:46:27 +0700

    update-12

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   const formatTime = (time) => {
     const getSeconds = `0${(time % 60)}`.slice(-2)
     const minutes = `${Math.floor(time/60)}`
-    const getMinutes = `${minutes % 60}`.slice(-2)
-    const getHours = `0${Math.floor(time/3600)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+    const getHours = `0${Math.floor(time/3600)}`.slice(-2)
 
     return `${getHours} : ${getMinutes} : ${getSeconds}`
   }
